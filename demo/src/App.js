9f4ad26cf49a039bd67dfd3bd55fdc628627e957
Chung Leong <chernyshevsky@hotmail.com>
2023-03-08 15:59:57 +0100

    Updated demo to use library.

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { BarcodeScanner } from './react-barcode-detection';
+import { BarcodeScanner } from 'react-barcode-detection';
 import './css/App.css';
 
 const bb = {
