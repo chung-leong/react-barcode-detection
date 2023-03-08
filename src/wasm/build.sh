09839eadacf5a1f5a12a6e22682c7328f28e2c6d
Chung Leong <chernyshevsky@hotmail.com>
2023-03-08 01:50:51 +0100

    Increased emcc optimization level to O3

--- src/wasm/build.sh
+++ src/wasm/build.sh
@@ -2,4 +2,4 @@
 cd quirc
 QUIRC_MAX_REGIONS=65534 emmake make libquirc.a
 cd ..
-emcc quirc.c -s ENVIRONMENT=worker -s MODULARIZE=1 -L./quirc -lquirc -o quirc.js
+emcc quirc.c -s ENVIRONMENT=worker -s MODULARIZE=1 -L./quirc -lquirc -O3 -o quirc.js
